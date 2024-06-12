ba38aa29ab6a5b3ed88760f16ef658df4b40f362
Vitaly <vitaly.amos@clixifix.co.uk>
2024-06-12 16:41:33 +0100

    changed DB and more

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_06_09_141216) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "width"
@@ -27,7 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_09_141216) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "order"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.integer "category_id"
     t.datetime "due_date"
     t.index ["user_id"], name: "index_tasks_on_user_id"
